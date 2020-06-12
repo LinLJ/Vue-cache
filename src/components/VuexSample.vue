ffdb481c7cd8d69dcb56cfac59425a05023ebb8c
linlj <linlj@mail.taiji.com.cn>
2020-06-12 17:10:56 +0800

    #

---
+++ src/components/VuexSample.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <el-button round type="info" @click="sampleClick">
+            点击
+        </el-button>
+    </div>
+
+</template>
+
+<script>
+  export default {
+    name: "VuexSample",
+    data() {
+      return {
+        message: "123123"
+      }
+    },
+    created(){
+      this.cookieMethod()
+    },
+    methods: {
+      cookieMethod(){
+        this.$cookies.set("cookie-test", "12")   //return this
+      },
+      sampleClick(){
+        //测试cookie
+        //console.info(this.$cookies.get("cookie-test"))
+        //console.info(this.$cookies.keys())
+
+        //测试store
+        console.log(this.$store.getters.doneTodos) // -> 1
+        console.log(this.$store.getters.doneTodosCount) // -> 1
+        console.log(this.$store.getters.getTodoById(2)) // -> 1
+
+      }
+
+    }
+  }
+</script>
+
+
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <VuexSample />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import VuexSample from "@/components/VuexSample.vue";
+
+export default {
+  name: "Home",
+  components: {
+    VuexSample
+  }
+};
+</script>
